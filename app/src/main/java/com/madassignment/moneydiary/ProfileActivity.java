954c5ed9a4a1dc0ed7cf8e44abaadd50baccc5f9
koee <72812317+Mryi007@users.noreply.github.com>
2021-03-06 04:15:31 +0800

    U

--- app/src/main/java/com/madassignment/moneydiary/ProfileActivity.java
+++ app/src/main/java/com/madassignment/moneydiary/ProfileActivity.java
@@ -1,13 +1,14 @@
 package com.madassignment.moneydiary;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener{
+    Button edtBtn,subBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +16,9 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         setContentView(R.layout.activity_profile_activity);
 
         Button editBtn = findViewById(R.id.editProfileBtn);
-
+        subBtn = findViewById(R.id.submitBtn);
         editBtn.setOnClickListener(this);
+        subBtn.setOnClickListener(this);
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.frameLayout2, new NonEditableUserData());
         ft.commit();
@@ -26,7 +28,24 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
     public void onClick(View v) {
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         if(v.getId() == R.id.editProfileBtn){
+            edtBtn = findViewById(R.id.editProfileBtn);
+            edtBtn.setVisibility(View.GONE);
+
+            subBtn = findViewById(R.id.submitBtn);
+            subBtn.setVisibility(View.VISIBLE);
+
             ft.replace(R.id.frameLayout2, new EditableUserData());
+            ft.commit();
+        }
+
+        if(v.getId() == R.id.submitBtn){
+            edtBtn = findViewById(R.id.editProfileBtn);
+            edtBtn.setVisibility(View.VISIBLE);
+
+            subBtn = findViewById(R.id.submitBtn);
+            subBtn.setVisibility(View.GONE);
+
+            ft.replace(R.id.frameLayout2, new NonEditableUserData());
             ft.commit();
         }
     }
