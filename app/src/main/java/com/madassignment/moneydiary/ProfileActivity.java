11317da58bc56a2bb76b436d2e6df82a2d470b3a
Kingsleyyong <kingsleyyong0208@gmail.com>
2021-03-06 03:30:35 +0800

    Profile Two Fragment without finishing the button sequence

--- app/src/main/java/com/madassignment/moneydiary/ProfileActivity.java
+++ app/src/main/java/com/madassignment/moneydiary/ProfileActivity.java
@@ -1,14 +1,33 @@
 package com.madassignment.moneydiary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_activity);
+
+        Button editBtn = findViewById(R.id.editProfileBtn);
+
+        editBtn.setOnClickListener(this);
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.frameLayout2, new NonEditableUserData());
+        ft.commit();
+    }
+
+    @Override
+    public void onClick(View v) {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        if(v.getId() == R.id.editProfileBtn){
+            ft.replace(R.id.frameLayout2, new EditableUserData());
+            ft.commit();
+        }
     }
 }
